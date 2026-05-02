5c70a1aa6302d578905436cf52420142dced07e9
oblzzz <163219091+oblzzz@users.noreply.github.com>
2026-05-02 01:28:25 -0300

    galeria

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ overlay.addEventListener('click', () => {
   nav.classList.remove('active');
   overlay.classList.remove('active');
 });
+
+// GALERIA 
+
+const images = document.querySelectorAll('.gallery-track img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = lightbox.querySelector('img');
+
+images.forEach(img => {
+  img.addEventListener('click', () => {
+    lightbox.classList.add('active');
+    lightboxImg.src = img.src;
+  });
+});
+
+lightbox.addEventListener('click', () => {
+  lightbox.classList.remove('active');
+});
